c0a399c2fd5ab3fc9e120737c3ec500c4938f034
samuelfa <samuelfa@kth.se>
2023-09-12 17:00:13 +0200

    Begin designing demo website

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,59 +1,109 @@
-<script>
-	import Counter from './Counter.svelte';
-	import welcome from '$lib/images/svelte-welcome.webp';
-	import welcome_fallback from '$lib/images/svelte-welcome.png';
+<script lang="ts" context="module">
+	export type Track = {
+		name: string;
+		artists: string[];
+		album: string;
+		year: number;
+		image: {
+			height: number;
+			width: number;
+			url: string;
+		};
+		preview: string | null;
+		uri: string;
+	};
 </script>
 
-<svelte:head>
-	<title>Home</title>
-	<meta name="description" content="Svelte demo app" />
-</svelte:head>
+<script lang="ts">
+	import TrackCard from './TrackCard.svelte';
+	let left = 1950;
+	let right = 2020;
+	const client_secret = '';
+	const user_token = '';
+	const refresh_token = '';
+	let minimize_card = false;
+	let selected_track: Track | undefined = {
+		album: 'Cut To The Feeling',
+		artists: ['Carly Rae Jepsen'],
+		image: {
+			height: 640,
+			url: 'https://i.scdn.co/image/ab67616d0000b2737359994525d219f64872d3b1',
+			width: 640
+		},
+		year: 2002,
+		name: 'Cut To The Feeling',
+		preview:
+			'https://p.scdn.co/mp3-preview/425d081433204c6ded4ae8f852ee89e5d5bcdc0d?cid=fd1121824a964f25a3f1ede3f782e0cf',
+		uri: 'spotify:track:11dFghVXANMlKmJXsNCbNl'
+	};
+	async function get_song(left: number, right: number) {
+		const year = Math.floor(Math.random() * (right - left) + left);
+		const offset = Math.floor(Math.random() * 1000);
+		let url = `https://api.spotify.com/v1/search?q=year:${year}&type=track&limit=5&offset=${offset}`;
+		let res = await fetch(url, {
+			method: 'GET',
+			headers: {
+				'content-type': 'application/json',
+				Authorization: `Bearer ${user_token}`
+			}
+		});
+		const data = await res.json();
+		const track = data.tracks.items.find((track: any) => track.preview_url != null);
+		if (track == undefined) {
+			console.log('No track found');
+			return;
+		}
+		selected_track = {
+			name: track.name,
+			artists: track.artists.map((artist: any) => artist.name),
+			album: track.album.name,
+			year: year,
+			image: track.album.images[0],
+			preview: track.preview_url,
+			uri: track.uri
+		};
+	}
+</script>
 
 <section>
-	<h1>
-		<span class="welcome">
-			<picture>
-				<source srcset={welcome} type="image/webp" />
-				<img src={welcome_fallback} alt="Welcome" />
-			</picture>
-		</span>
+	<div>
+		<h1>Demo Page for Spotify Stuff</h1>
+		<div>
+			<h3>Select a Year Interval</h3>
+			<h4>Left: {left}, Right: {right}</h4>
+			<input bind:value={left} type="range" min="1950" max="2020" step="5" />
+			<input bind:value={right} type="range" min="1950" max="2020" step="5" />
+			<div>
+				<button on:click={async () => await get_song(left, right)}>Get Song</button>
+				<button on:click={() => (minimize_card = !minimize_card)}>Minimize Card</button>
+			</div>
+		</div>
+	</div>
 
-		to your new<br />SvelteKit app
-	</h1>
-
-	<h2>
-		try editing <strong>src/routes/+page.svelte</strong>
-	</h2>
-
-	<Counter />
+	{#if selected_track !== undefined}
+		<TrackCard track={selected_track} minimized={minimize_card} />
+	{/if}
 </section>
 
 <style>
 	section {
 		display: flex;
-		flex-direction: column;
+		flex-direction: row;
 		justify-content: center;
 		align-items: center;
 		flex: 0.6;
 	}
 
+	section > div {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		width: fit-content;
+		margin: 5em;
+	}
+
 	h1 {
 		width: 100%;
-	}
-
-	.welcome {
-		display: block;
-		position: relative;
-		width: 100%;
-		height: 0;
-		padding: 0 0 calc(100% * 495 / 2048) 0;
-	}
-
-	.welcome img {
-		position: absolute;
-		width: 100%;
-		height: 100%;
-		top: 0;
-		display: block;
 	}
 </style>
---
+++ src/routes/TrackCard.svelte
@@ -0,0 +1,60 @@
+<script lang="ts">
+	import type { Track } from './+page.svelte';
+	export let track: Track;
+	export let minimized: boolean;
+	console.log(track);
+</script>
+
+<section>
+	<div class="container">
+		<h1>Selected Track:</h1>
+		<div class="card">
+			<img src={track.image.url} alt="Album Artwork" />
+            <div class="track-info">
+				<p class="year">{track.year}</p>
+				{#if !minimized}
+					<p class="text">{track.name} - {track.artists[0]}</p>
+				{/if}
+            </div>
+			<audio autoplay style="width: 100%">
+				<source src={track.preview} type="audio/mpeg" />
+			</audio>
+		</div>
+	</div>
+</section>
+
+<style>
+	.card {
+		position: relative;
+		display: flex;
+		flex-direction: column;
+		width: 450px;
+		padding: 0;
+		--radius: 20px;
+		border-radius: var(--radius);
+		box-shadow: 0 0 20px rgba(0, 0, 0, 0.5);
+		--card-bg: rgb(60, 60, 60);
+        background-color: var(--card-bg);
+		color: rgb(240, 240, 240);
+	}
+	.card img {
+        width: 100%;
+		border-radius: var(--radius) var(--radius) 0 0;
+		
+	}
+	.year {
+        font-size: xx-large;
+		font-weight: bold;
+		z-index: 2;
+	}
+	.text {
+		font-size: large;
+		z-index: -1;
+	}
+    .track-info {
+		text-align: center;
+        padding-left: 4em;
+		padding-right: 4em;
+		border-radius: var(--radius);
+    }
+</style>
