2f6e6eefd4081951faefd835fe726d42e6ecffbd
samuelfa <samuelfa@kth.se>
2023-10-10 12:20:08 +0200

    Structure mobile routes

---
+++ src/routes/(mobile-controller)/play/+page.svelte
@@ -0,0 +1,44 @@
+<script lang="ts">
+	import { getContext } from 'svelte';
+	import type { MainContext } from '../+layout.svelte';
+	import WaitingScreen from './WaitingScreen.svelte';
+	import EndGame from './EndGame.svelte';
+	import ControllerScreen from './ControllerScreen.svelte';
+
+	const { socket, myName } = getContext<MainContext>('main');
+    
+    let gameStarted = false;
+    let gameEnded = false;
+	let myTurn = false;
+
+	socket.on('startGame', () => {
+		console.log('Game started!');
+		gameStarted = true;
+	});
+
+	socket.on('endGame', () => {
+		console.log('Game ended!');
+		gameEnded = true;
+	});
+
+	socket.on('error', (error) => {
+		alert(error);
+	});
+
+	socket.on('assignTurn', ({ userId }) => {
+		if (userId === socket.id) {
+			console.log('My turn');
+			myTurn = true;
+		} else {
+			myTurn = true;
+		}
+	});
+</script>
+
+{#if gameEnded}
+    <EndGame {socket}/>
+{:else if gameStarted}
+    <ControllerScreen {socket} {myTurn}/>
+{:else}
+    <WaitingScreen {myName}/>
+{/if}
--- src/routes/(mobile-controller)/play/ControllerScreen.svelte
+++ src/routes/(mobile-controller)/play/ControllerScreen.svelte
@@ -1,13 +1,13 @@
 <script lang="ts">
-	export let socket: any;
-	export let roomIdVariable: string;
-	export let nameVariable: string;
+	import type { ClientToServerEvents, ServerToClientEvents } from "$lib/socketServer";
+	import type { Socket } from "socket.io-client";
+
+	export let socket: Socket<ServerToClientEvents, ClientToServerEvents>;
 	export let myTurn: boolean;
-	export let setMyTurn: (bool: boolean) => void;
 
 	let answer: number;
 	const startYear = 1950;
-	const endYear = 2020;
+	const endYear = 2023;
 	const years = Array(endYear - startYear + 1).fill(null).map((_, index) => startYear + index);
 
 	function send() {
@@ -15,8 +15,7 @@
 			alert('Please select a year.');
 			return;
 		}
-		socket.emit('submitAnswer', { roomId: roomIdVariable, answer: answer.toString() , name: nameVariable});
-		setMyTurn(false);
+		socket.emit('submitAnswer', { answer });
 	}
 </script>
 
@@ -37,5 +36,6 @@
 		</div>
     {:else}
 		<h1 class="text-2xl md:text-4xl mb-4">Waiting for other players to submit their answers...</h1>
+		<div class="loading loading-dots loading-xl"></div>
     {/if}
 </div>
