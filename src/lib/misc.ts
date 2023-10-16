69b756fe0bd6c29d975eb56499cd379855d67c45
Jonas Hulthén <jonas.hulthen@live.se>
2023-10-17 00:24:00 +0200

    dockergit add .

--- src/lib/misc.ts
+++
@@ -1,6 +0,0 @@
-
-
-export function randomRoomId() {
-	return (Math.random() + 1).toString(36).substring(2, 7);
-}
-
--- src/routes/spotify/newToken/+page.svelte
+++
@@ -1,7 +0,0 @@
-<script lang="ts">
-    import { goto } from '$app/navigation';
-    import { accessToken, type Tokens } from '$stores/tokenStore';
-    export let data: Tokens;
-    accessToken.setTokens(data);
-    goto('/')
-</script>
